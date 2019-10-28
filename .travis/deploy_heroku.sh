1a444aca2d706f0b87770e443411f50445b56797
Alex Hosseini <ahosseini7@my.bcit.ca>
2019-10-28 11:54:39 -0700

    Update deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-# heroku plugins:install heroku-container-registry
-#docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
-docker login -u _ --password=8a43cc1d-96dc-4c41-a73e-7a99eafb3ca1 registry.heroku.com
-heroku container:push web --app serene-caverns-87756
-heroku container:release web -app serene-caverns-87756
+heroku plugins:install heroku-container-registry
+docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
